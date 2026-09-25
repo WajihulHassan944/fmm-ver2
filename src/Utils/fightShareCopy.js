bdda35a79a68cd6a2ff84df986723cb7d2b039cc
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2026-09-25 09:24:18 -0400

    Include affiliate fight link in Instagram caption (#105)

--- src/Utils/fightShareCopy.js
+++ src/Utils/fightShareCopy.js
@@ -15,7 +15,7 @@ export const affiliateFightPosts = (title, link, league = 'my league', prize = 0
   const xSuffix = `. Make your picks and compete for prizes where eligible. ${link} Affiliate link; I may earn from entries.`;
   return {
     facebook: `${invitation}\n\nJoin my league through my personal fight link: ${facebookLink}\n\n${disclosure}\n#FANTASYMMADNESS #FightNight`,
-    instagram: `${invitation}\n\nScan my personal QR on the poster to join my league.\n\n${disclosure}\n#FANTASYMMADNESS #FightNight`,
+    instagram: `${invitation}\n\nJOIN MY LEAGUE: ${link}\nScan my personal QR on the poster to open this link.\n\n${disclosure}\n#FANTASYMMADNESS #FightNight`,
     tiktok: `${invitation}\n\nScan my personal QR to join and play with me.\n\n${disclosure}\n#FANTASYMMADNESS #FightTok`,
     x: `${xPrefix}${name.slice(0, Math.max(0, 280 - xPrefix.length - xSuffix.length))}${xSuffix}`,
   };
